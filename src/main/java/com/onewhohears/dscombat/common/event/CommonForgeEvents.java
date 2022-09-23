3cc82e7f8a12c382ee0b66cdf44841b3d61e0485
1whohears <1whohears28@gmail.com>
2022-09-22 23:35:38 -0500

    still no

--- src/main/java/com/onewhohears/dscombat/common/event/CommonForgeEvents.java
+++ src/main/java/com/onewhohears/dscombat/common/event/CommonForgeEvents.java
@@ -2,7 +2,7 @@ package com.onewhohears.dscombat.common.event;
 
 import com.onewhohears.dscombat.entity.aircraft.parts.EntitySeat;
 
-import net.minecraft.world.entity.EntityDimensions;
+import net.minecraft.world.phys.AABB;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
@@ -19,9 +19,14 @@ public class CommonForgeEvents {
 		if (player == null) return;
 		System.out.println("server side vehicle "+player.getVehicle());
 		if (!(player.getVehicle() instanceof EntitySeat seat)) return;
-		EntityDimensions dim = new EntityDimensions(player.getBbWidth(), 0.5f, true);
-		player.setBoundingBox(dim.makeBoundingBox(player.position()));
+		//EntityDimensions dim = new EntityDimensions(player.getBbWidth(), 0.5f, true);
+		//player.setBoundingBox(dim.makeBoundingBox(player.position()));
 		System.out.println("server side player hitbox");
+		double x = player.getX();
+		double y = player.getY();
+		double z = player.getZ();
+		double w = player.getBbWidth()/2;
+		player.setBoundingBox(new AABB(x+w, y, z+w, x-w, y+0.5, z-w));
 	}
 	
 }

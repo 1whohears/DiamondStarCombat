94c18443184c97b7b72c2dc3c764f1b1879376b0
1whohears <1whohears28@gmail.com>
2022-10-14 17:27:06 -0500

    Update TestWeaponCommand.java

--- src/main/java/com/onewhohears/dscombat/command/TestWeaponCommand.java
+++ src/main/java/com/onewhohears/dscombat/command/TestWeaponCommand.java
@@ -1,25 +1,35 @@
 package com.onewhohears.dscombat.command;
 
+import java.util.Collection;
+
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
+import net.minecraft.commands.arguments.EntityArgument;
+import net.minecraft.server.level.ServerPlayer;
 
 public class TestWeaponCommand {
 	
 	public TestWeaponCommand(CommandDispatcher<CommandSourceStack> d) {
 		d.register(Commands.literal("testweapon").requires(null).requires((stack) -> {return stack.hasPermission(2);})
-			.then(Commands.literal("missile").executes((context) -> {
-				return 0;
-			})
-		));
+			.then(Commands.literal("missile")
+			.then(Commands.argument("target", EntityArgument.players())
+			.executes((context) -> {
+				return testMissile(context, EntityArgument.getPlayers(context, "target"));
+			}
+		))));
 	}
 	
-	private int testMissile(CommandContext source) throws CommandSyntaxException {
-		
-		return 1;
+	private int testMissile(CommandContext<CommandSourceStack> context, Collection<ServerPlayer> players) throws CommandSyntaxException {
+		int i = 0;
+		for (ServerPlayer p : players) {
+			
+			++i;
+		}
+		return i;
 	}
 	
 }

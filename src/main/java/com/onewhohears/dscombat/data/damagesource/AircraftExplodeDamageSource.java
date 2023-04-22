7bb4fbc271d6fce8f61d5697cf712f596e7e5f42
1whohears <1whohears28@gmail.com>
2023-04-22 18:46:25 -0500

    fixed plane crash death messages

--- src/main/java/com/onewhohears/dscombat/data/damagesource/AircraftExplodeDamageSource.java
+++ src/main/java/com/onewhohears/dscombat/data/damagesource/AircraftExplodeDamageSource.java
@@ -2,13 +2,19 @@ package com.onewhohears.dscombat.data.damagesource;
 
 import com.onewhohears.dscombat.entity.aircraft.EntityAircraft;
 
-import net.minecraft.world.damagesource.EntityDamageSource;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
 
-public class AircraftExplodeDamageSource extends EntityDamageSource {
-
+public class AircraftExplodeDamageSource extends DamageSource {
+	
+	public final EntityAircraft aircraft;
+	
 	public AircraftExplodeDamageSource(String type, EntityAircraft aircraft) {
-		super(type, aircraft);
+		super(type);
 		setExplosion();
+		this.aircraft = aircraft;
 	}
 	
 	public static AircraftExplodeDamageSource fall(EntityAircraft aircraft) {
@@ -17,6 +23,18 @@ public class AircraftExplodeDamageSource extends EntityDamageSource {
 	
 	public static AircraftExplodeDamageSource collide(EntityAircraft aircraft) {
 		return new AircraftExplodeDamageSource("dscombat.plane_crash_collide", aircraft);
+	}
+	
+	public Component getLocalizedDeathMessage(LivingEntity killed) {
+		Entity killer = aircraft.getControllingPassenger();
+		String s = "death.attack."+msgId;
+		if (killer == null) {
+			return Component.translatable(s, killed.getDisplayName());
+		} else if (killed.equals(killer)) {
+			return Component.translatable(s+".self", killed.getDisplayName());
+		} else {
+			return Component.translatable(s+".player", killed.getDisplayName(), killer.getDisplayName());
+		}
 	}
 
 }

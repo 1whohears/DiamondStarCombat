628edcf52b2274a2de2c492dc63adcff8d34c696
1whohears <1whohears28@gmail.com>
2023-05-15 20:11:43 -0500

    model setup for new vehicles

---
+++ models/wooden_plane/wooden_plane.java
@@ -0,0 +1,33 @@
+// Made with Blockbench 4.6.5
+// Exported for Minecraft version 1.17 or later with Mojang mappings
+// Paste this class into your mod and generate all required imports
+
+
+public class wooden_plane<T extends Entity> extends EntityModel<T> {
+	// This layer location should be baked with EntityRendererProvider.Context in the entity renderer and passed into this model's constructor
+	public static final ModelLayerLocation LAYER_LOCATION = new ModelLayerLocation(new ResourceLocation("modid", "wooden_plane"), "main");
+	
+
+	public wooden_plane(ModelPart root) {
+		
+	}
+
+	public static LayerDefinition createBodyLayer() {
+		MeshDefinition meshdefinition = new MeshDefinition();
+		PartDefinition partdefinition = meshdefinition.getRoot();
+
+		
+
+		return LayerDefinition.create(meshdefinition, 16, 16);
+	}
+
+	@Override
+	public void setupAnim(T entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
+
+	}
+
+	@Override
+	public void renderToBuffer(PoseStack poseStack, VertexConsumer vertexConsumer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
+		
+	}
+}

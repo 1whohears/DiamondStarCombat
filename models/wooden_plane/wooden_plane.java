6f40acd498c21042415b12b50e43a694fdc49546
1whohears <1whohears28@gmail.com>
2023-05-17 23:54:52 -0500

    finished wooden plane

--- models/wooden_plane/wooden_plane.java
+++ models/wooden_plane/wooden_plane.java
@@ -22,21 +22,21 @@ public class wooden_plane<T extends Entity> extends EntityModel<T> {
 
 		PartDefinition wing = fuselage.addOrReplaceChild("wing", CubeListBuilder.create(), PartPose.offset(0.0F, 0.0F, 0.0F));
 
-		PartDefinition left_wing = wing.addOrReplaceChild("left_wing", CubeListBuilder.create().texOffs(0, 43).addBox(-41.0F, -1.0F, -7.0F, 32.0F, 1.0F, 14.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 0.0F, 0.0F));
+		PartDefinition left_wing = wing.addOrReplaceChild("left_wing", CubeListBuilder.create().texOffs(91, 17).addBox(-41.0F, -1.0F, -7.0F, 32.0F, 1.0F, 14.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 0.0F, 0.0F));
 
-		PartDefinition right_wing = wing.addOrReplaceChild("right_wing", CubeListBuilder.create().texOffs(86, 0).addBox(9.0F, -1.0F, -7.0F, 32.0F, 1.0F, 14.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 0.0F, 0.0F));
+		PartDefinition right_wing = wing.addOrReplaceChild("right_wing", CubeListBuilder.create().texOffs(91, 1).addBox(9.0F, -1.0F, -7.0F, 32.0F, 1.0F, 14.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 0.0F, 0.0F));
 
 		PartDefinition frame = fuselage.addOrReplaceChild("frame", CubeListBuilder.create().texOffs(0, 0).addBox(-6.0F, -6.0F, -40.0F, 12.0F, 12.0F, 31.0F, new CubeDeformation(0.0F))
-		.texOffs(92, 43).addBox(-9.0F, 5.0F, -8.0F, 18.0F, 1.0F, 16.0F, new CubeDeformation(0.0F))
-		.texOffs(86, 83).addBox(8.0F, -6.0F, -8.0F, 1.0F, 11.0F, 16.0F, new CubeDeformation(0.0F))
-		.texOffs(52, 83).addBox(-9.0F, -6.0F, -8.0F, 1.0F, 11.0F, 16.0F, new CubeDeformation(0.0F))
-		.texOffs(0, 123).addBox(-9.0F, -6.0F, -9.0F, 18.0F, 12.0F, 1.0F, new CubeDeformation(0.0F))
-		.texOffs(0, 110).addBox(-9.0F, -6.0F, 8.0F, 18.0F, 12.0F, 1.0F, new CubeDeformation(0.0F))
-		.texOffs(66, 60).addBox(-6.0F, -6.0F, 9.0F, 12.0F, 12.0F, 11.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 0.0F, 0.0F));
+		.texOffs(101, 35).addBox(-9.0F, 5.0F, -8.0F, 18.0F, 1.0F, 16.0F, new CubeDeformation(0.0F))
+		.texOffs(87, 77).addBox(8.0F, -6.0F, -8.0F, 1.0F, 11.0F, 16.0F, new CubeDeformation(0.0F))
+		.texOffs(49, 78).addBox(-9.0F, -6.0F, -8.0F, 1.0F, 11.0F, 16.0F, new CubeDeformation(0.0F))
+		.texOffs(7, 96).addBox(-9.0F, -6.0F, -9.0F, 18.0F, 12.0F, 1.0F, new CubeDeformation(0.0F))
+		.texOffs(7, 79).addBox(-9.0F, -6.0F, 8.0F, 18.0F, 12.0F, 1.0F, new CubeDeformation(0.0F))
+		.texOffs(61, 47).addBox(-6.0F, -6.0F, 9.0F, 12.0F, 12.0F, 11.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 0.0F, 0.0F));
 
-		PartDefinition tail = fuselage.addOrReplaceChild("tail", CubeListBuilder.create().texOffs(0, 83).addBox(-22.0F, -1.0F, -40.0F, 16.0F, 1.0F, 10.0F, new CubeDeformation(0.0F))
-		.texOffs(112, 60).addBox(6.0F, -1.0F, -40.0F, 16.0F, 1.0F, 10.0F, new CubeDeformation(0.0F))
-		.texOffs(38, 110).addBox(-0.5F, -18.0F, -40.0F, 1.0F, 12.0F, 10.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 0.0F, 0.0F));
+		PartDefinition tail = fuselage.addOrReplaceChild("tail", CubeListBuilder.create().texOffs(4, 48).addBox(-22.0F, -1.0F, -40.0F, 16.0F, 1.0F, 10.0F, new CubeDeformation(0.0F))
+		.texOffs(5, 62).addBox(6.0F, -1.0F, -40.0F, 16.0F, 1.0F, 10.0F, new CubeDeformation(0.0F))
+		.texOffs(116, 58).addBox(-0.5F, -18.0F, -40.0F, 1.0F, 12.0F, 10.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 0.0F, 0.0F));
 
 		PartDefinition seat = body.addOrReplaceChild("seat", CubeListBuilder.create(), PartPose.offset(0.0F, 0.0F, 0.0F));
 
@@ -52,8 +52,8 @@ public class wooden_plane<T extends Entity> extends EntityModel<T> {
 		.texOffs(0, 145).addBox(-1.0F, 5.0F, -1.0F, 2.0F, 2.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 6.0F, -26.5F));
 
 		PartDefinition propeller = body.addOrReplaceChild("propeller", CubeListBuilder.create().texOffs(42, 143).addBox(-1.0F, -1.0F, 0.0F, 2.0F, 2.0F, 2.0F, new CubeDeformation(0.0F))
-		.texOffs(0, 60).addBox(-16.0F, -1.0F, 2.0F, 32.0F, 2.0F, 1.0F, new CubeDeformation(0.0F))
-		.texOffs(60, 110).addBox(-1.0F, -16.0F, 2.0F, 2.0F, 32.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 0.0F, 20.0F));
+		.texOffs(6, 115).addBox(-16.0F, -1.0F, 2.0F, 32.0F, 2.0F, 1.0F, new CubeDeformation(0.0F))
+		.texOffs(83, 116).addBox(-1.0F, -16.0F, 2.0F, 2.0F, 32.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 0.0F, 20.0F));
 
 		return LayerDefinition.create(meshdefinition, 256, 256);
 	}

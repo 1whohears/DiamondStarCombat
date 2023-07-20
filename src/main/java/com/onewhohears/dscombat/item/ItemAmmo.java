c646c83958931d2437598a41d07bfb1fc7fe97ab
1whohears <1whohears28@gmail.com>
2023-07-20 15:30:36 -0500

    improved throttle

--- src/main/java/com/onewhohears/dscombat/item/ItemAmmo.java
+++ src/main/java/com/onewhohears/dscombat/item/ItemAmmo.java
@@ -1,5 +1,9 @@
 package com.onewhohears.dscombat.item;
 
+import java.util.List;
+
+import javax.annotation.Nullable;
+
 import com.onewhohears.dscombat.data.weapon.WeaponData;
 import com.onewhohears.dscombat.data.weapon.WeaponPresets;
 import com.onewhohears.dscombat.init.ModItems;
@@ -11,6 +15,8 @@ import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public class ItemAmmo extends Item {
@@ -35,6 +41,12 @@ public class ItemAmmo extends Item {
 			test.setTag(tag);
 			items.add(test);
 		}
+	}
+	
+	@Override
+	public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> tips, TooltipFlag isAdvanced) {
+		super.appendHoverText(stack, level, tips, isAdvanced);
+		// TODO 3.1 show weapon stats
 	}
 	
 	@Override

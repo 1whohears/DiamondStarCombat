afe2f38e7ea8486938f5ed31b2893680ad276eef
1whohears <1whohears28@gmail.com>
2023-09-21 11:57:43 -0500

    removed comments

--- src/main/java/com/onewhohears/dscombat/data/parts/PartSlot.java
+++ src/main/java/com/onewhohears/dscombat/data/parts/PartSlot.java
@@ -28,10 +28,6 @@ public class PartSlot {
 	private PartData data;
 	
 	public PartSlot(CompoundTag entityNbt, @Nullable CompoundTag presetNbt) {
-		//System.out.println("READING NEW PART SLOT");
-		//System.out.println("entity = "+entityNbt.toString());
-		//if (presetNbt != null) System.out.println("preset = "+presetNbt.toString());
-		//else System.out.println("preset = null");
 		slotId = entityNbt.getString("name");
 		locked = entityNbt.getBoolean("locked");
 		if (entityNbt.contains("data")) data = UtilParse.parsePartFromCompound(entityNbt.getCompound("data"));
@@ -41,7 +37,6 @@ public class PartSlot {
 		} else type = SlotType.getByName(presetNbt.getString("slot_type"));
 		pos = UtilParse.readVec3(presetNbt, "slot_pos");
 		zRot = presetNbt.getFloat("zRot");
-		//System.out.println("pos = "+pos);
 	}
 	
 	public CompoundTag write() {
@@ -56,17 +51,12 @@ public class PartSlot {
 	}
 	
 	public PartSlot(FriendlyByteBuf buffer) {
-		//System.out.println("PART SLOT BUFFER");
 		slotId = buffer.readUtf();
-		//System.out.println("name = "+name);
 		type = SlotType.getByName(buffer.readUtf());
-		//System.out.println("type = "+type.name());
 		pos = DataSerializers.VEC3.read(buffer);
-		//System.out.println("pos = "+pos);
 		zRot = buffer.readFloat();
 		locked = buffer.readBoolean();
 		boolean notNull = buffer.readBoolean();
-		//System.out.println("notNull = "+notNull);
 		if (notNull) data = DataSerializers.PART_DATA.read(buffer);
 	}
 	

b2508b5e3c7ebfeb31dd79fc3c2af7a23a2379f4
1whohears <1whohears28@gmail.com>
2023-09-26 15:17:09 -0500

    custom hitbox wip code

---
+++ src/main/java/com/onewhohears/dscombat/entity/aircraft/RotableHitbox.java
@@ -0,0 +1,184 @@
+package com.onewhohears.dscombat.entity.aircraft;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+import com.mojang.math.Quaternion;
+import com.onewhohears.dscombat.util.math.RotableAABB;
+import com.onewhohears.dscombat.util.math.UtilAngles;
+
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.protocol.Packet;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityDimensions;
+import net.minecraft.world.entity.Pose;
+import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.Vec3;
+import net.minecraftforge.entity.PartEntity;
+
+public class RotableHitbox extends PartEntity<EntityAircraft> {
+	
+	private final String name;
+	private final RotableAABB hitbox;
+	private final EntityDimensions size;
+	private final Vec3 rel_pos;
+	private final float precision;
+	private SubCollider[] subColliders;
+	
+	public RotableHitbox(EntityAircraft parent, String name, Vec3 size, Vec3 rel_pos, float precision) {
+		super(parent);
+		this.name = name;
+		this.hitbox = new RotableAABB(size.x, size.y, size.z);
+		this.size = hitbox.getMaxDimensions();
+		this.rel_pos = rel_pos;
+		this.precision = precision;
+		this.noPhysics = true;
+		refreshDimensions();
+		createSubColliders();
+		positionSubColliders();
+	}
+	
+	@Override
+	public void tick() {
+		positionSelf();
+		positionSubColliders();
+		positionEntities();
+		firstTick = false;
+	}
+	
+	protected void createSubColliders() {
+		List<Vec3> pos = hitbox.createRelPosList(getPrecision());
+		subColliders = new SubCollider[pos.size()];
+		for (int i = 0; i < pos.size(); ++i) subColliders[i] = new SubCollider(
+				this, i, getPrecision(), pos.get(i));
+	}
+	
+	protected void positionSelf() {
+		Quaternion q;
+		if (level.isClientSide) q = getParent().getClientQ();
+		else q = getParent().getQ();
+		Vec3 pos = getParent().position().add(UtilAngles.rotateVector(getRelPos(), q));
+		setPos(pos);
+		hitbox.setCenter(pos);
+		hitbox.setRot(q);
+	}
+	
+	protected void positionSubColliders() {
+		for (int i = 0; i < subColliders.length; ++i) 
+			subColliders[i].setPos(hitbox.repositionSubCollider(subColliders[i].getRelPos()));
+	}
+	
+	protected void positionEntities() {
+		Vec3 parent_move = getParent().getDeltaMovement();
+		Vec3 parent_rot = getParent().getAngularVel();
+		List<Entity> list = level.getEntities(this, getBoundingBox(), canMoveEntity());
+		for (Entity entity : list) {
+			Vec3 entity_move = entity.getDeltaMovement().add(parent_move);
+			entity_move.add(hitbox.getTangetVel(
+				entity.position().subtract(hitbox.getCenter()), 
+				parent_rot));
+			entity.setDeltaMovement(entity_move);
+		}
+	}
+	
+	public Predicate<? super Entity> canMoveEntity() {
+		return (entity) -> {
+			if (entity.noPhysics) return false;
+			if (!entity.verticalCollisionBelow) return false;
+			AABB groundBox = entity.getBoundingBox().setMaxY(0.1).move(0, -0.1, 0);
+			List<Entity> sub_list = level.getEntities(entity, groundBox, 
+					(e) -> {return e instanceof SubCollider;});
+			return sub_list.size() != 0;
+		};
+	}
+	
+	@Override
+    public boolean hurt(DamageSource source, float amount) {
+    	return getParent().hurt(source, amount);
+    }
+	
+	@Override
+    protected AABB makeBoundingBox() {
+		double pX = getX(), pY = getY(), pZ = getZ();
+    	EntityDimensions d = getDimensions(getPose());
+    	double f = d.width / 2.0F;
+        double f1 = d.height / 2.0F;
+        return new AABB(pX-f, pY-f1, pZ-f, 
+        		pX+f, pY+f1, pZ+f);
+    }
+	
+	public String getHitboxName() {
+		return name;
+	}
+	
+	public RotableAABB getHitbox() {
+		return hitbox;
+	}
+	
+	public Vec3 getRelPos() {
+		return rel_pos;
+	}
+	
+	public float getPrecision() {
+		return precision;
+	}
+	
+	@Override
+	public boolean isPickable() {
+		return false;
+	}
+	
+	@Override
+    public boolean isPushable() {
+    	return false;
+    }
+    
+    @Override
+    public boolean isPushedByFluid() {
+    	return false;
+    }
+
+    @Override
+    public boolean canBeCollidedWith() {
+        return false;
+    }
+    
+    @Override
+    public boolean canCollideWith(Entity entity) {
+    	return false;
+    }
+	
+    @Override
+	public EntityDimensions getDimensions(Pose pPose) {
+		return size;
+	}
+    
+	@Override
+	public boolean is(Entity entity) {
+		return this == entity || getParent() == entity;
+	}
+	
+	@Override
+	public Packet<?> getAddEntityPacket() {
+		throw new UnsupportedOperationException();
+	}
+	
+	@Override
+	public boolean shouldBeSaved() {
+		return false;
+	}
+
+	@Override
+	protected void defineSynchedData() {
+	}
+
+	@Override
+	protected void readAdditionalSaveData(CompoundTag nbt) {
+	}
+
+	@Override
+	protected void addAdditionalSaveData(CompoundTag nbt) {
+	}
+
+}
--- src/main/java/com/onewhohears/dscombat/util/math/RotableAABB.java
+++ src/main/java/com/onewhohears/dscombat/util/math/RotableAABB.java
@@ -1,7 +1,11 @@
 package com.onewhohears.dscombat.util.math;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.mojang.math.Quaternion;
 
+import net.minecraft.world.entity.EntityDimensions;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
 
@@ -32,8 +36,33 @@ public class RotableAABB {
 		return new RotableAABB(center, extents, rot);
 	}
 	
+	public List<Vec3> createRelPosList(float precision) {
+		double height = extents.y*2/precision;
+		double length = extents.z*2/precision;
+		double width = extents.x*2/precision;
+		int hsteps = (int)Math.ceil(height);
+		int lsteps = (int)Math.ceil(length);
+		int wsteps = (int)Math.ceil(width);
+		List<Vec3> list = new ArrayList<Vec3>();
+		for(int h=0;h<hsteps;++h)for(int l=0;l<lsteps;++l)for(int w=0;w<wsteps;++w) {
+			double hpos = h*precision-extents.y-(height-hsteps)/2;
+			double lpos = l*precision-extents.z-(length-lsteps)/2;
+			double wpos = w*precision-extents.x-(width-wsteps)/2;
+			list.add(new Vec3(hpos, lpos, wpos));
+		}
+		return list;
+	}
+	
+	public Vec3 repositionSubCollider(Vec3 rel_pos) {
+		return UtilAngles.rotateVector(rel_pos, rot).add(getCenter());
+	}
+	
+	public Vec3 getTangetVel(Vec3 rel_pos, Vec3 ang_vel) {
+		
+	}
+	
 	public static Vec3 extentsFromBB(AABB bb) {
-		return new Vec3(bb.getXsize() / 2, bb.getYsize() / 2, bb.getZsize() / 2);
+		return new Vec3(bb.getXsize()/2, bb.getYsize()/2, bb.getZsize()/2);
 	}
 	
 	public Quaternion getRot() {
@@ -54,6 +83,11 @@ public class RotableAABB {
 
 	public void move(Vec3 offset) {
 		setCenter(getCenter().add(offset));
+	}
+	
+	public EntityDimensions getMaxDimensions() {
+		float max = (float)Math.max(extents.x, Math.max(extents.y, extents.z));
+		return EntityDimensions.scalable(max, max);
 	}
 	
 }

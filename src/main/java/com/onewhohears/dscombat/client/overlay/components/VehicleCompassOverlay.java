3daf8a9d8c698d4611f2744ab4a975e54e2967aa
kawaiicakes <vomitmopper@gmail.com>
2023-10-18 03:43:16 -0600

    Overhauled compass

--- src/main/java/com/onewhohears/dscombat/client/overlay/components/VehicleCompassOverlay.java
+++ src/main/java/com/onewhohears/dscombat/client/overlay/components/VehicleCompassOverlay.java
@@ -1,15 +1,26 @@
 package com.onewhohears.dscombat.client.overlay.components;
 
+import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.onewhohears.dscombat.client.overlay.VehicleOverlayComponent;
 import com.onewhohears.dscombat.entity.aircraft.EntityVehicle;
 import net.minecraft.client.Minecraft;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.Mth;
 
 import java.util.Objects;
 
-// FIXME: FPS does NOT like this. drawCenteredString seems to be the culprit
+import static com.onewhohears.dscombat.DSCombatMod.MODID;
+
 public class VehicleCompassOverlay extends VehicleOverlayComponent {
+    public static final ResourceLocation COMPASS = new ResourceLocation(MODID,
+            "textures/ui/hud_compass.png");
+
+    public static final short COMPASS_TEXTURE_WIDTH = 720;
+    public static final byte COMPASS_TEXTURE_HEIGHT = 16;
+    public static final short COMPASS_U_WIDTH = 161;
+    public static final byte COMPASS_CORRECTIONAL_OFFSET = 30;
+
     private static VehicleCompassOverlay INSTANCE;
 
     public static void renderIfAllowed(PoseStack poseStack, int screenWidth, int screenHeight) {
@@ -25,36 +36,41 @@ public class VehicleCompassOverlay extends VehicleOverlayComponent {
 
         // HEADING
         int y = 10, color = 0xe6e600;
-        int heading = (int) Mth.wrapDegrees(Minecraft.getInstance().player.getYRot());
-        if (heading < 0) heading += (int) 360f;
-        drawCenteredString(poseStack, getFont(),
-                heading+"", screenWidth/2, y+20, color);
-        int num = 15, degSpace = 3, degPerLine = 3, steps = 3;
-        for (int i = -num; i < num; ++i) {
-            int j = i*degPerLine;
-            int x = screenWidth/2+j*degSpace-heading%degPerLine*degSpace;
-            drawCenteredString(poseStack, getFont(),
-                    "|", x, y+10, color);
-            int hl = heading / degPerLine;
-            if ((hl+i) % steps != 0) continue;
-            int h = hl*degPerLine+j;
-            if (h < 0) h += 360;
-            else if (h >= 360) h-= 360;
-            drawCenteredString(poseStack, getFont(),
-                    textByHeading(h), x, y, color);
-        }
-        // TODO 1.2 draw pitch, roll, and cursor elements to hud
+        float heading = (Mth.wrapDegrees(Minecraft.getInstance().player.getYRot()));
+        if (heading < 0) heading += 360f;
+        String stringHeading = !Objects.equals(textByHeading((int) heading), "")
+                ? textByHeading((int) heading)
+                : String.valueOf((int) heading);
+
+        drawCenteredString(poseStack, getFont(), stringHeading,
+                screenWidth / 2, y + 40, color);
+
+        RenderSystem.enableBlend();
+        RenderSystem.setShaderTexture(0, COMPASS);
+
+        poseStack.pushPose();
+        poseStack.translate(((double) (screenWidth - COMPASS_U_WIDTH) / 2) - 1, 30, 0);
+
+        blit(poseStack,
+                0, 0,
+                (Minecraft.getInstance().player.getYRot() * 2) - COMPASS_CORRECTIONAL_OFFSET, 0,
+                COMPASS_U_WIDTH, COMPASS_TEXTURE_HEIGHT,
+                COMPASS_TEXTURE_WIDTH, COMPASS_TEXTURE_HEIGHT);
+
+        poseStack.popPose();
     }
 
     private static String textByHeading(int h) {
-        if (h == 0) return "S";
-        else if (h == 180) return "N";
-        else if (h == 90) return "W";
-        else if (h == 270) return "E";
-        else if (h == 45) return "SW";
-        else if (h == 135) return "NW";
-        else if (h == 225) return "NE";
-        else if (h == 315) return "SE";
-        return h+"";
+        return switch (h) {
+            case 0 -> "S";
+            case 180 -> "N";
+            case 90 -> "W";
+            case 270 -> "E";
+            case 45 -> "SW";
+            case 135 -> "NW";
+            case 225 -> "NE";
+            case 315 -> "SE";
+            default -> "";
+        };
     }
 }

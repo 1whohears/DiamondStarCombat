31af815e6fe9b416d2e1e94fec1f978330f0c86f
kawaiicakes <vomitmopper@gmail.com>
2023-10-18 01:23:09 -0600

    Added plane stats to HUD

--- src/main/java/com/onewhohears/dscombat/client/overlay/components/HudOverlay.java
+++ src/main/java/com/onewhohears/dscombat/client/overlay/components/HudOverlay.java
@@ -5,8 +5,10 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.math.Vector3f;
 import com.onewhohears.dscombat.client.overlay.VehicleOverlayComponent;
 import com.onewhohears.dscombat.entity.aircraft.EntityPlane;
+import com.onewhohears.dscombat.util.UtilEntity;
 import net.minecraft.resources.ResourceLocation;
 
+import java.awt.*;
 import java.util.Objects;
 
 import static com.onewhohears.dscombat.DSCombatMod.MODID;
@@ -15,6 +17,14 @@ import static com.onewhohears.dscombat.client.event.forgebus.ClientInputEvents.M
 public class HudOverlay extends VehicleOverlayComponent {
     public static final ResourceLocation HUD = new ResourceLocation(MODID,
             "textures/ui/hud_overlay.png");
+
+    public static final Color GREEN_ME_SAY_ALONE_RAMP = new Color(0, 255, 0);
+    public static final Color RED = new Color(255, 0, 0);
+    public static final float START = 0.6f;
+    public static final float END = 0.1f;
+    public static final float CHANGE_G = (float) GREEN_ME_SAY_ALONE_RAMP.getGreen() / (START - END);
+    public static final float CHANGE_R = (float) RED.getRed() / (START - END);
+
     public static final short HUD_TEXTURE_WIDTH = 320;
     public static final short HUD_TEXTURE_HEIGHT = 294;
     public static final byte VERTICAL_BOUNDS_WIDTH = 125;
@@ -45,6 +55,8 @@ public class HudOverlay extends VehicleOverlayComponent {
         if (!(getPlayerVehicle() instanceof EntityPlane plane)) return;
         if (!MOUSE_MODE || plane.onlyFreeLook()) return;
 
+        drawStrings(poseStack, screenWidth, screenHeight, plane);
+
         RenderSystem.setShaderTexture(0, HUD);
         RenderSystem.enableDepthTest();
         RenderSystem.enableBlend();
@@ -60,6 +72,41 @@ public class HudOverlay extends VehicleOverlayComponent {
 
         RenderSystem.disableBlend();
         RenderSystem.disableDepthTest();
+    }
+
+    /**
+     * It's important to call this first as weird visual fuckshit happens otherwise
+     */
+    private static void drawStrings(PoseStack poseStack, int screenWidth, int screenHeight, EntityPlane plane) {
+        poseStack.pushPose();
+        poseStack.translate(((double) screenWidth / 2) + 23, ((double) (screenHeight + VERTICAL_BOUNDS_HEIGHT) / 2) - 25, 0);
+        poseStack.scale(0.7F, 0.7F, 1);
+
+        drawString(poseStack, getFont(),
+                String.format("AOA: %3.1f", plane.getAOA()),
+                0, 0,
+                0x00ff00);
+
+        poseStack.popPose();
+        poseStack.pushPose();
+        poseStack.translate(((double) screenWidth / 2) -50, ((double) (screenHeight + VERTICAL_BOUNDS_HEIGHT) / 2) - 25, 0);
+        poseStack.scale(0.7F, 0.7F, 1);
+
+        drawString(poseStack, getFont(),
+                "m/s: " + String.format("%3.1f", plane.getDeltaMovement().length() * 20),
+                0, 0,
+                0x00ff00);
+
+        drawString(poseStack, getFont(),
+                "A: " + UtilEntity.getDistFromSeaLevel(plane),
+                0, 10,
+                0x00ff00);
+
+        drawString(poseStack, getFont(),
+                "[" + plane.getBlockX() + "," + plane.getBlockY() + "," + plane.getBlockZ() + "]",
+                0, 20, 0x00ff00);
+
+        poseStack.popPose();
     }
 
     // FIXME: different resolutions/screen sizes will cause these values to not line up properly (usually by one or two pixels)
@@ -95,5 +142,17 @@ public class HudOverlay extends VehicleOverlayComponent {
                 xOrigin, yOrigin,
                 ((number * 7) - (number - 1)) + HORIZONTAL_BOUNDS_U_OFFSET, 0,
                 NUMBERS_UV_WIDTH, NUMBERS_UV_HEIGHT);
+    }
+
+    private static int getHealthColor(float health, float max) {
+        float healthPercent = health / max;
+        if (healthPercent >= START) return GREEN_ME_SAY_ALONE_RAMP.getRGB();
+        if (healthPercent < START && healthPercent > END) {
+            return new Color(
+                    (int)(CHANGE_R *(START -healthPercent)),
+                    (int)(CHANGE_G *(healthPercent- END)),
+                    0).getRGB();
+        }
+        return RED.getRGB();
     }
 }

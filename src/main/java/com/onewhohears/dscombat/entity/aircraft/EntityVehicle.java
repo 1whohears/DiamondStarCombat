a3d0601583f554ac03d99b3a79c01330534f3e0a
1whohears <1whohears28@gmail.com>
2023-11-17 12:52:47 -0600

    TODO

--- src/main/java/com/onewhohears/dscombat/entity/aircraft/EntityVehicle.java
+++ src/main/java/com/onewhohears/dscombat/entity/aircraft/EntityVehicle.java
@@ -179,7 +179,6 @@ public abstract class EntityVehicle extends Entity implements IEntityAdditionalS
 	protected RotableHitbox[] hitboxes = new RotableHitbox[0];
 	protected EntityScreenData[] screens = new EntityScreenData[0];
 	
-	// FIXME 1.1 fix aircraft texture/variant texture system
 	// TODO 5.4 aircraft breaks apart when damaged
 	// TODO 5.6 place and remove external parts from outside the vehicle
 	// TODO 2.5 add chaff

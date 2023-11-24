0012c31f7403f5b4fb8ae9ea9475b377ab73b3f6
kawaiicakes <vomitmopper@gmail.com>
2023-11-24 13:09:52 -0700

    NoahChopper now has a rudimentary HUD screen.

--- src/main/java/com/onewhohears/dscombat/client/renderer/texture/HudScreenInstance.java
+++ src/main/java/com/onewhohears/dscombat/client/renderer/texture/HudScreenInstance.java
@@ -1,18 +1,28 @@
 package com.onewhohears.dscombat.client.renderer.texture;
 
-import com.onewhohears.dscombat.entity.aircraft.EntityPlane;
+import com.onewhohears.dscombat.data.aircraft.EntityScreenData;
+import com.onewhohears.dscombat.entity.aircraft.EntityVehicle;
 import net.minecraft.client.Minecraft;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.phys.Vec3;
 
 import static com.onewhohears.dscombat.DSCombatMod.MODID;
+import static com.onewhohears.dscombat.client.renderer.texture.EntityScreenTypes.HUD_SCREEN;
 
 public class HudScreenInstance extends EntityDynamicScreenInstance {
+    public static final EntityScreenData DEFAULT_DATA = new EntityScreenData(
+            HUD_SCREEN,
+            new Vec3(0.39895, 0.62, 1.63),
+            0.1f, 0.1f,
+            0, 0, 0
+    );
+
     protected int renderDelay;
 
     public static final ResourceLocation HUD_BASE = new ResourceLocation(MODID,
-            "textures/ui/hud_overlay_base");
+            "textures/ui/hud_overlay_base.png");
 
     public HudScreenInstance(int id) {
         super("hud", id, HUD_BASE);
@@ -22,11 +32,11 @@ public class HudScreenInstance extends EntityDynamicScreenInstance {
     @Override
     public boolean shouldUpdateTexture(Entity entity) {
         if (Minecraft.getInstance().player == null) return false;
-        if (!(entity instanceof EntityPlane plane)) return false;
+        if (!(entity instanceof EntityVehicle vehicle)) return false;
 
         this.renderDelay++;
-        // if no players are in the plane the HUD should not update as frequently to reduce strain on the client render thread
-        return plane.hasPassenger(entityPredicate -> entityPredicate instanceof Player)
+        // if no players are in the vehicle the HUD should not update as frequently to reduce strain on the client render thread
+        return vehicle.hasPassenger(entityPredicate -> entityPredicate instanceof Player)
                 || this.renderDelay % 10 == 0;
     }
 
--- src/main/java/com/onewhohears/dscombat/entity/aircraft/custom/NoahChopper.java
+++ src/main/java/com/onewhohears/dscombat/entity/aircraft/custom/NoahChopper.java
@@ -1,16 +1,18 @@
 package com.onewhohears.dscombat.entity.aircraft.custom;
 
-import com.onewhohears.dscombat.client.renderer.texture.EntityScreenTypes;
+import com.onewhohears.dscombat.client.renderer.texture.HudScreenInstance;
 import com.onewhohears.dscombat.data.aircraft.EntityScreenData;
 import com.onewhohears.dscombat.data.aircraft.ImmutableVehicleData;
 import com.onewhohears.dscombat.data.aircraft.ImmutableVehicleData.Builder;
 import com.onewhohears.dscombat.data.aircraft.presets.HeliPresets;
 import com.onewhohears.dscombat.entity.aircraft.EntityHelicopter;
 import com.onewhohears.dscombat.init.ModSounds;
-
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
+
+import static com.onewhohears.dscombat.client.renderer.texture.EntityScreenTypes.HUD_SCREEN;
+import static com.onewhohears.dscombat.client.renderer.texture.EntityScreenTypes.RADAR_SCREEN;
 
 public class NoahChopper extends EntityHelicopter {
 	
@@ -33,11 +35,12 @@ public class NoahChopper extends EntityHelicopter {
 	
 	@Override
 	public void addVehicleScreens() {
-		screens = new EntityScreenData[1];
-		screens[0] = new EntityScreenData(EntityScreenTypes.RADAR_SCREEN, 
+		screens = new EntityScreenData[2];
+		screens[0] = new EntityScreenData(RADAR_SCREEN,
 				new Vec3(0.35, 0.05, 1.83), 
 				0.6f, 0.6f, 
 				20f, 0f, 0f);
+		screens[1] = HudScreenInstance.DEFAULT_DATA;
 	}
 
 }

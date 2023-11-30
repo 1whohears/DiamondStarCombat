d9c55f84c52b53bc27ffee85cd77079e8d37c518
1whohears <1whohears28@gmail.com>
2023-11-30 13:52:47 -0600

    DSCClientInputs docs

--- src/main/java/com/onewhohears/dscombat/client/input/DSCClientInputs.java
+++ src/main/java/com/onewhohears/dscombat/client/input/DSCClientInputs.java
@@ -14,47 +14,66 @@ public class DSCClientInputs {
 	private static long mountTime;
 	
 	private static MouseMode CURRENT_MOUSE_MODE = MouseMode.FREE_RELATIVE;
-	
+	/**
+	 * set mouseCenterX and mouseCenterY to the mouse's current position.
+	 * used to move the "joystick" back to the middle when in LOCKED_FORWARD MouseMode.
+	 */
 	public static void centerMousePos() {
 		Minecraft m = Minecraft.getInstance();
 		mouseCenterX = m.mouseHandler.xpos();
 		mouseCenterY = m.mouseHandler.ypos();
 	}
-	
+	/**
+	 * @return the mouse x position related to the "joystick's" origin when in LOCKED_FORWARD MouseMode.
+	 */
 	public static double getMouseCenterX() {
 		return mouseCenterX;
 	}
-	
+	/**
+	 * @return the mouse y position related to the "joystick's" origin when in LOCKED_FORWARD MouseMode.
+	 */
 	public static double getMouseCenterY() {
 		return mouseCenterY;
 	}
-	
-	public static double setMouseCenterX(double x) {
+	/**
+	 * @param x the mouse x position related to the "joystick's" origin when in LOCKED_FORWARD MouseMode.
+	 */
+	public static void setMouseCenterX(double x) {
 		mouseCenterX = x;
-		return mouseCenterX;
 	}
-	
-	public static double setMouseCenterY(double y) {
+	/**
+	 * @param y the mouse y position related to the "joystick's" origin when in LOCKED_FORWARD MouseMode.
+	 */
+	public static void setMouseCenterY(double y) {
 		mouseCenterY = y;
-		return mouseCenterY;
 	}
-	
+	/**
+	 * @return the index of the ping the client's mouse is hovering over. -1 if {@link DSCClientInputs#isRadarHovering} is true.
+	 */
 	public static int getRadarHoverIndex() {
 		return hoverIndex;
 	}
-	
+	/**
+	 * @param index the index of the ping the client's mouse is hovering over
+	 */
 	public static void setRadarHoverIndex(int index) {
 		hoverIndex = index;
 	}
-	
+	/**
+	 * called if the client's mouse isn't hovering over any pings on the hud
+	 */
 	public static void resetRadarHoverIndex() {
 		hoverIndex = -1;
 	}
-	
+	/**
+	 * @return is the client's mouse hovering over a radar ping on the hud
+	 */
 	public static boolean isRadarHovering() {
 		return hoverIndex != -1;
 	}
-	
+	/**
+	 * @return the max distance of a radar ping client radar screens will display
+	 */
 	public static double getRadarDisplayRange() {
 		return radarDisplayRange;
 	}
@@ -62,19 +81,28 @@ public class DSCClientInputs {
 	public static void setRadarDisplayRange(double range) {
 		radarDisplayRange = range;
 	}
-	
+	/**
+	 * @return the last time in millis the client mounted a vehicle
+ 	 */
 	public static long getClientMountTime() {
 		return mountTime;
 	}
-	
+	/**
+	 * @param time the last time in millis the client mounted a vehicle
+	 */
 	public static void setClientMountTime(long time) {
 		mountTime = time;
 	}
-	
+	/**
+	 * @return {@link MouseMode#FREE_RELATIVE}, {@link MouseMode#FREE_GLOBAL}, or {@link MouseMode#LOCKED_FORWARD}
+	 */
 	public static MouseMode getMouseMode() {
 		return CURRENT_MOUSE_MODE;
 	}
-	
+	/**
+	 * sets CURRENT_MOUSE_MODE to the next MouseMode option.
+	 * @return the new current Mouse Mode option.
+	 */
 	public static MouseMode cycleMouseMode() {
 		CURRENT_MOUSE_MODE = CURRENT_MOUSE_MODE.cycle();
 		return CURRENT_MOUSE_MODE;
@@ -96,8 +124,18 @@ public class DSCClientInputs {
 	}
 	
 	public static enum MouseMode {
+		/**
+		 * Camera can move freely but turns when the vehicle turns.
+		 * Keeps the camera's angle the same relative angle to the vehicle. 
+		 */
 		FREE_RELATIVE, // FIXME 2.2 give MouseMode FREE_RELATIVE functionality
+		/**
+		 * Camera moves freely. Is not effected by the vehicle's rotation.
+		 */
 		FREE_GLOBAL,
+		/**
+		 * Camera is locked towards the vehicle's forward direction.
+		 */
 		LOCKED_FORWARD;
 		public MouseMode cycle() {
 			int index = ordinal();

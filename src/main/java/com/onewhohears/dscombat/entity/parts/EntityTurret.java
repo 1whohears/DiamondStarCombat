9fbb369af96572b6ac8fc7cccf267a7b12145243
1whohears <1whohears28@gmail.com>
2024-07-23 22:06:24 -0500

    Update EntityTurret.java

--- src/main/java/com/onewhohears/dscombat/entity/parts/EntityTurret.java
+++ src/main/java/com/onewhohears/dscombat/entity/parts/EntityTurret.java
@@ -25,7 +25,6 @@ import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.util.Mth;
-import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;

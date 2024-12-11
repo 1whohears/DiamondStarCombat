b2e157f73c339be479d654e28d0f492ad01acad6
1whohears <1whohears28@gmail.com>
2024-12-11 14:39:32 -0600

    added some vehicle tags for compat

--- src/main/java/com/onewhohears/dscombat/data/tag/EntityTypeTagGen.java
+++ src/main/java/com/onewhohears/dscombat/data/tag/EntityTypeTagGen.java
@@ -36,7 +36,9 @@ public class EntityTypeTagGen extends EntityTypeTagsProvider {
 			.addOptional(new ResourceLocation("cgm:projectile"))
 			.addOptional(new ResourceLocation("moguns:taki"))
 			.addOptional(new ResourceLocation("moguns:magma_cream"))
-			.addOptional(new ResourceLocation("moguns:flare"));
+			.addOptional(new ResourceLocation("moguns:flare"))
+				.addOptional(new ResourceLocation("tacz:bullet"))
+				.addOptional(new ResourceLocation("smallships:cannon_ball"));
 		tag(ModTags.EntityTypes.BOMB)
 			.add(ModEntities.BOMB.get(), ModEntities.BUNKER_BUSTER.get())
 			.addOptional(new ResourceLocation("cgm:grenade"))
@@ -71,9 +73,12 @@ public class EntityTypeTagGen extends EntityTypeTagsProvider {
 		tag(ModTags.EntityTypes.CAR)
 			.add(ModEntities.CAR.get());
 		tag(ModTags.EntityTypes.TANK)
-			.add(ModEntities.CAR.get());
+			.add(ModEntities.CAR.get())
+				.addOptional(new ResourceLocation("car:car"));
 		tag(ModTags.EntityTypes.BOAT)
-			.add(ModEntities.BOAT.get());
+			.add(ModEntities.BOAT.get())
+				.addOptional(new ResourceLocation("smallships:cog"))
+				.addOptional(new ResourceLocation("smallships:brigg"));
 		tag(ModTags.EntityTypes.SUBMARINE)
 			.add(ModEntities.SUBMARINE.get());
 		// TURRET

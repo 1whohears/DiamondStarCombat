7e27d0760d65894676d2ab50da99fed559da213e
1whohears <1whohears28@gmail.com>
2025-01-14 17:41:12 -0600

    adjusted scale and rotation overrides of obj item models

--- src/main/java/com/onewhohears/dscombat/client/model/obj/custom/AATurretModel.java
+++ src/main/java/com/onewhohears/dscombat/client/model/obj/custom/AATurretModel.java
@@ -1,9 +1,7 @@
 package com.onewhohears.dscombat.client.model.obj.custom;
 
 import com.google.common.collect.ImmutableMap;
-import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.math.Matrix4f;
-import com.mojang.math.Vector3f;
 import com.onewhohears.dscombat.client.model.obj.ObjTurretModel;
 import com.onewhohears.dscombat.entity.parts.EntityTurret;
 import com.onewhohears.onewholibs.util.math.UtilAngles;
@@ -21,25 +19,12 @@ public class AATurretModel extends ObjTurretModel<EntityTurret> {
 		float yrothead = UtilAngles.lerpAngle180(partialTicks, entity.yRotRelO, entity.getRelRotY());
 		float xrothead = UtilAngles.lerpAngle(partialTicks, entity.xRotRelO, entity.getRelRotX());
 		Matrix4f yrothead_mat = UtilAngles.pivotRotY(0, 0, 0, -yrothead);
-		Matrix4f xrothead_mat = UtilAngles.pivotPixelsRotZ(-7, 16.5f, 0, xrothead);
-		Matrix4f seat_mat = yrothead_mat.copy(), gun_mat = yrothead_mat.copy(), 
-				barrel1_mat = yrothead_mat.copy(), barrel2_mat = yrothead_mat.copy();
-		gun_mat.multiply(xrothead_mat);
-		barrel1_mat.multiply(xrothead_mat);
-		barrel2_mat.multiply(xrothead_mat);
+		Matrix4f xrothead_mat = UtilAngles.pivotPixelsRotX(-7, 16.5f, 0, xrothead);
 		ImmutableMap<String, Matrix4f> transforms = ImmutableMap.<String, Matrix4f>builder()
-			.put("seat", seat_mat)
-			.put("gun", gun_mat)
-			.put("barrel1", barrel1_mat)
-			.put("barrel2", barrel2_mat)
+			.put("seat", yrothead_mat)
+			.put("gun", xrothead_mat)
 			.build();
 		return Transforms.of(transforms);
-	}
-	
-	@Override
-	protected void rotate(EntityTurret entity, float partialTicks, PoseStack poseStack) {
-		super.rotate(entity, partialTicks, poseStack);
-		poseStack.mulPose(Vector3f.YP.rotationDegrees(90f));
 	}
 
 }

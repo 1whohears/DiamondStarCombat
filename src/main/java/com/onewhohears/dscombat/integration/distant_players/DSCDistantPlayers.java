ab6030a3560939c7bfc73f0a326433ded733e557
1whohears <1whohears28@gmail.com>
2025-04-05 14:23:51 -0500

    added dsc vehicle plugin for render distant players

--- src/main/java/com/onewhohears/dscombat/integration/distant_players/DSCDistantPlayers.java
+++ src/main/java/com/onewhohears/dscombat/integration/distant_players/DSCDistantPlayers.java
@@ -1,11 +1,17 @@
 package com.onewhohears.dscombat.integration.distant_players;
 
 import com.onewhohears.distant_players.client.core.ExtraInfoManager;
+import com.onewhohears.dscombat.init.ModEntities;
 
 public class DSCDistantPlayers {
 
     public static void register() {
-        //ExtraInfoManager.register();
+        ExtraInfoManager.register(ModEntities.BOAT.get(), DSCExtraRenderInfo::new);
+        ExtraInfoManager.register(ModEntities.CAR.get(), DSCExtraRenderInfo::new);
+        ExtraInfoManager.register(ModEntities.PLANE.get(), DSCExtraRenderInfo::new);
+        ExtraInfoManager.register(ModEntities.HELICOPTER.get(), DSCExtraRenderInfo::new);
+        ExtraInfoManager.register(ModEntities.SUBMARINE.get(), DSCExtraRenderInfo::new);
+        ExtraInfoManager.register(ModEntities.STATIONARY.get(), DSCExtraRenderInfo::new);
     }
 
 }

64fd5613f53c9d641f6ab0625856845a5a8a0fae
1whohears <79810154+1whohears@users.noreply.github.com>
2025-07-24 00:37:12 -0500

    gamerules begin with dscombat: to make them easier to find in the commands

--- src/main/java/com/onewhohears/dscombat/entity/weapon/EntityBullet.java
+++ src/main/java/com/onewhohears/dscombat/entity/weapon/EntityBullet.java
@@ -1,5 +1,6 @@
 package com.onewhohears.dscombat.entity.weapon;
 
+import com.onewhohears.dscombat.command.DSCGameRules;
 import com.onewhohears.dscombat.data.vehicle.physics.DSCPhyCons;
 import com.onewhohears.dscombat.data.weapon.WeaponType;
 import com.onewhohears.dscombat.data.weapon.stats.BulletStats;
@@ -40,7 +41,8 @@ public class EntityBullet<T extends BulletStats> extends EntityWeapon<T> {
 		if (!level.hasChunk(chunkPosition().x, chunkPosition().z)) return;
 		if (!level.isClientSide && getExplosive()) {
 			Explosion.BlockInteraction interact = Explosion.BlockInteraction.NONE;
-			if (getTerrain()) interact = Explosion.BlockInteraction.BREAK;
+			if (getTerrain() && !getLevel().getGameRules().getBoolean(DSCGameRules.WEAPONS_BREAK_BLOCKS))
+				interact = Explosion.BlockInteraction.BREAK;
 			for (int i = 0; i < getExplodeNum(); ++i) {
 				level.explode(this, getExplosionDamageSource(),
 					null, getX(), getY(), getZ(), 

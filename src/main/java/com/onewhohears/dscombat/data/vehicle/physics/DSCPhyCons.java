c3e0b6e5bc82c9148f4b7cab7f8617879805119c
1whohears <79810154+1whohears@users.noreply.github.com>
2025-08-24 11:36:32 -0500

    increased max fall speed

--- src/main/java/com/onewhohears/dscombat/data/vehicle/physics/DSCPhyCons.java
+++ src/main/java/com/onewhohears/dscombat/data/vehicle/physics/DSCPhyCons.java
@@ -63,7 +63,7 @@ public class DSCPhyCons {
 	public static final float COLLIDE_SPEED_GEAR = 1.5f;
 	public static final float COLLIDE_DAMAGE_RATE = 300f;
 	
-	public static final double MAX_FALL_SPEED = 2.5;
+	public static final double MAX_FALL_SPEED = 5;
 	public static final double MAX_CLIMB_SPEED = 2.5;
 	public static final double MAX_HELICOPTER_CLIMB_SPEED = 1.0;
 	

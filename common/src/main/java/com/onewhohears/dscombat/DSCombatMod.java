2c37dd7a0a5544260ffc0cefc3645e549340e758
1whohears <79810154+1whohears@users.noreply.github.com>
2025-09-20 13:28:04 -0500

    fixed some client events

--- common/src/main/java/com/onewhohears/dscombat/DSCombatMod.java
+++ common/src/main/java/com/onewhohears/dscombat/DSCombatMod.java
@@ -1,6 +1,12 @@
 package com.onewhohears.dscombat;
 
+import com.onewhohears.dscombat.client.event.ClientEventHandlers;
+import com.onewhohears.dscombat.client.model.obj.HardCodedModelAnims;
 import com.onewhohears.dscombat.common.event.CommonEventHandlers;
+import com.onewhohears.dscombat.data.parts.client.PartAssets;
+import com.onewhohears.dscombat.data.sound.VehiclePassengerSoundPacks;
+import com.onewhohears.dscombat.data.vehicle.client.VehicleClientPresets;
+import com.onewhohears.dscombat.data.weapon.client.WeaponAssets;
 import com.onewhohears.dscombat.entity.vehicle.EntityVehicle;
 import com.onewhohears.dscombat.init.DataSerializers;
 import com.onewhohears.dscombat.init.ModArgumentTypes;
@@ -17,6 +23,8 @@ import com.onewhohears.dscombat.init.ModTags;
 import com.onewhohears.dscombat.init.ModVillagers;
 
 import dev.architectury.platform.Platform;
+import dev.architectury.registry.ReloadListenerRegistry;
+import net.minecraft.server.packs.PackType;
 
 /**
  * This is the main class of the Diamond Star Combat mod.
@@ -59,7 +67,12 @@ public class DSCombatMod {
     }
 
     public static void clientInit() {
-
+        ClientEventHandlers.init();
+        HardCodedModelAnims.reload();
+        ReloadListenerRegistry.register(PackType.CLIENT_RESOURCES, VehicleClientPresets.get());
+        ReloadListenerRegistry.register(PackType.CLIENT_RESOURCES, PartAssets.get());
+        ReloadListenerRegistry.register(PackType.CLIENT_RESOURCES, WeaponAssets.get());
+        ReloadListenerRegistry.register(PackType.CLIENT_RESOURCES, VehiclePassengerSoundPacks.get());
     }
 
     public DSCombatMod() {
--- common/src/main/java/com/onewhohears/dscombat/client/event/ClientEventHandlers.java
+++ common/src/main/java/com/onewhohears/dscombat/client/event/ClientEventHandlers.java
@@ -1,4 +1,109 @@
 package com.onewhohears.dscombat.client.event;
 
+import com.onewhohears.dscombat.client.entityscreen.EntityScreenIds;
+import com.onewhohears.dscombat.client.entityscreen.EntityScreenTypes;
+import com.onewhohears.dscombat.client.entityscreen.instance.*;
+import com.onewhohears.dscombat.client.model.obj.customanims.DSCAnimControl;
+import com.onewhohears.dscombat.client.model.obj.customanims.VehicleModelTransforms;
+import com.onewhohears.dscombat.client.particle.*;
+import com.onewhohears.dscombat.client.screen.*;
+import com.onewhohears.dscombat.data.sound.PassengerSoundPack;
+import com.onewhohears.dscombat.init.ModContainers;
+import com.onewhohears.dscombat.init.ModFluids;
+import com.onewhohears.dscombat.init.ModParticles;
+import com.onewhohears.onewholibs.client.model.obj.customanims.CustomAnims;
+import com.onewhohears.onewholibs.client.model.obj.customanims.keyframe.ControllableAnimPlayer;
+import com.onewhohears.onewholibs.client.model.obj.customanims.keyframe.KFAnimPlayers;
+import dev.architectury.event.EventResult;
+import dev.architectury.event.events.client.ClientLifecycleEvent;
+import dev.architectury.event.events.common.ChatEvent;
+import dev.architectury.registry.client.particle.ParticleProviderRegistry;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screens.MenuScreens;
+import net.minecraft.client.renderer.ItemBlockRenderTypes;
+import net.minecraft.client.renderer.RenderType;
+
 public class ClientEventHandlers {
+
+    public static void init() {
+        ClientLifecycleEvent.CLIENT_SETUP.register(ClientEventHandlers::onClientSetup);
+    }
+
+    public static void onClientSetup(Minecraft minecraft) {
+        registerScreens();
+        registerCustomAnims();
+        registerKeyframeAnims();
+        setFluidRenderLayers();
+        registerEntityScreens();
+        PassengerSoundPack.registerBuiltInPassengerSoundTriggers();
+        registerParticleProvider();
+    }
+
+    public static void registerParticleProvider() {
+        ParticleProviderRegistry.register(ModParticles.LARGE_SMOKE_CLOUD.get(), LargeSmokeCloudParticle.Provider::new);
+        ParticleProviderRegistry.register(ModParticles.SHRAPNEL.get(), ShrapnelParticle.Provider::new);
+        ParticleProviderRegistry.register(ModParticles.BIG_FLAME.get(), BigFlameParticle.Provider::new);
+        ParticleProviderRegistry.register(ModParticles.CONTRAIL.get(), ContrailParticle.Provider::new);
+        ParticleProviderRegistry.register(ModParticles.AFTER_BURNER.get(), AfterBurnerParticle.Provider::new);
+        ParticleProviderRegistry.register(ModParticles.FLARE.get(), FlareParticle.Provider::new);
+    }
+
+    public static void registerEntityScreens() {
+        EntityScreenTypes.addScreenType(EntityScreenIds.BIG_RADAR_SCREEN, BigRadarScreenInstance::new, "008282");
+        EntityScreenTypes.addScreenType(EntityScreenIds.AIR_RADAR_SCREEN, AirRadarScreenInstance::new, "00FFFF");
+        EntityScreenTypes.addScreenType(EntityScreenIds.GROUND_RADAR_SCREEN, GroundRadarScreenInstance::new, "4CFF00");
+        EntityScreenTypes.addScreenType(EntityScreenIds.FUEL_SCREEN, FuelScreenInstance::new, "7F0000");
+        // TODO 1.2 what to do with hud screen?
+        EntityScreenTypes.addScreenType(EntityScreenIds.HUD_SCREEN, HudScreenInstance::new, "");
+        EntityScreenTypes.addScreenType(EntityScreenIds.RWR_SCREEN, RWRScreenInstance::new, "FF00DC");
+        EntityScreenTypes.addScreenType(EntityScreenIds.HEADING_SCREEN, HeadingScreenInstance::new, "0026FF");
+        EntityScreenTypes.addScreenType(EntityScreenIds.TURN_COORD_SCREEN, TurnCoordScreenInstance::new, "008718");
+        EntityScreenTypes.addScreenType(EntityScreenIds.ATTITUDE_SCREEN, AttitudeScreenInstance::new, "7F3300");
+        EntityScreenTypes.addScreenType(EntityScreenIds.AOA_SCREEN, AOAScreenInstance::new, "840084");
+        EntityScreenTypes.addScreenType(EntityScreenIds.ALTIMETER_SCREEN, AltimeterScreenInstance::new, "848400");
+        EntityScreenTypes.addScreenType(EntityScreenIds.AIR_SPEED_SCREEN, SpeedScreenInstance::new, "FFD800");
+    }
+
+    public static void setFluidRenderLayers() {
+        ItemBlockRenderTypes.setRenderLayer(ModFluids.OIL_FLUID_SOURCE.get(), RenderType.translucent());
+        ItemBlockRenderTypes.setRenderLayer(ModFluids.OIL_FLUID_FLOWING.get(), RenderType.translucent());
+    }
+
+    public static void registerKeyframeAnims() {
+        KFAnimPlayers.addAnimationPlayerFactory("turret_shoot", (data) -> new ControllableAnimPlayer<>(data,
+                DSCAnimControl.TURRET_SHOOT_TRIGGER, DSCAnimControl.TURRET_SHOOT_CONTROL));
+        KFAnimPlayers.addAnimationPlayerFactory("turret_shoot_loop", (data) -> new ControllableAnimPlayer<>(data,
+                DSCAnimControl.TURRET_SHOOT_LOOP_TRIGGER, DSCAnimControl.TURRET_SHOOT_LOOP_CONTROL));
+        KFAnimPlayers.addAnimationPlayerFactory("turret_shoot_loop_end", (data) -> new ControllableAnimPlayer<>(data,
+                DSCAnimControl.TURRET_SHOOT_LOOP_END_TRIGGER, DSCAnimControl.TURRET_SHOOT_LOOP_END_CONTROL));
+        KFAnimPlayers.addAnimationPlayerFactory("vehicle_landing_gear", (data) -> new ControllableAnimPlayer<>(data,
+                DSCAnimControl.LANDING_GEAR_TRIGGER, DSCAnimControl.LANDING_GEAR_CONTROL));
+    }
+
+    public static void registerCustomAnims() {
+        CustomAnims.addAnim("input_bound_translation", VehicleModelTransforms.InputBoundTranslation::new);
+        CustomAnims.addAnim("motor_rotation", VehicleModelTransforms.MotorRotation::new);
+        CustomAnims.addAnim("wheel_rotation", VehicleModelTransforms.WheelRotation::new);
+        CustomAnims.addAnim("input_bound_rotation", VehicleModelTransforms.InputBoundRotation::new);
+        CustomAnims.addAnim("spinning_radar", VehicleModelTransforms.SpinningRadar::new);
+        CustomAnims.addAnim("landing_gear", VehicleModelTransforms.LandingGear::new);
+        CustomAnims.addAnim("hitbox_destroy_part", VehicleModelTransforms.HitboxDestroyPart::new);
+        CustomAnims.addAnim("plane_flap_rotation", VehicleModelTransforms.PlaneFlapRotation::new);
+        CustomAnims.addAnim("turret_rotation", VehicleModelTransforms.TurretRotation::new);
+    }
+
+    public static void registerScreens() {
+        MenuScreens.register(ModContainers.VEHICLE_PARTS_MENU.get(), VehiclePartsScreen::new);
+        MenuScreens.register(ModContainers.WEAPONS_BLOCK_MENU.get(), WeaponsBlockScreen::new);
+        MenuScreens.register(ModContainers.WEAPON_PARTS_BLOCK_MENU.get(), WeaponPartsBlockScreen::new);
+        MenuScreens.register(ModContainers.AIRCRAFT_BLOCK_MENU.get(), VehicleBlockScreen::new);
+        MenuScreens.register(ModContainers.VEHICLE_STORAGE_MENU_9x0.get(), VehicleStorageScreen::new);
+        MenuScreens.register(ModContainers.VEHICLE_STORAGE_MENU_9x1.get(), VehicleStorageScreen::new);
+        MenuScreens.register(ModContainers.VEHICLE_STORAGE_MENU_9x2.get(), VehicleStorageScreen::new);
+        MenuScreens.register(ModContainers.VEHICLE_STORAGE_MENU_9x3.get(), VehicleStorageScreen::new);
+        MenuScreens.register(ModContainers.VEHICLE_STORAGE_MENU_9x4.get(), VehicleStorageScreen::new);
+        MenuScreens.register(ModContainers.VEHICLE_STORAGE_MENU_9x5.get(), VehicleStorageScreen::new);
+        MenuScreens.register(ModContainers.VEHICLE_STORAGE_MENU_9x6.get(), VehicleStorageScreen::new);
+    }
+
 }

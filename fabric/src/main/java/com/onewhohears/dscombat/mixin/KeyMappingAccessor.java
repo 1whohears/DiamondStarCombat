0813c41d3df512d479a8e4cd96ae8cae0128796d
1whohears <79810154+1whohears@users.noreply.github.com>
2025-12-14 11:35:08 -0600

    ALL | fixed fabric keybind system

---
+++ fabric/src/main/java/com/onewhohears/dscombat/mixin/KeyMappingAccessor.java
@@ -0,0 +1,16 @@
+package com.onewhohears.dscombat.mixin;
+
+import com.mojang.blaze3d.platform.InputConstants;
+import net.minecraft.client.KeyMapping;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+import java.util.Map;
+
+@Mixin(KeyMapping.class)
+public interface KeyMappingAccessor {
+    @Accessor("MAP")
+    static Map<InputConstants.Key, KeyMapping> getMap() {
+        throw new AssertionError();
+    }
+}

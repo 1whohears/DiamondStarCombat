63c3be5a262b50830731e8ff8a389e4d73ad095d
1whohears <79810154+1whohears@users.noreply.github.com>
2026-06-12 21:13:20 -0500

    ALL | all krait choppers now have a gimbal camera in the front (need to make it match the moddel that is already there)

--- common/src/main/java/com/onewhohears/dscombat/data/vehicle/presets/helicopter/KraitChopperPresets.java
+++ common/src/main/java/com/onewhohears/dscombat/data/vehicle/presets/helicopter/KraitChopperPresets.java
@@ -44,6 +44,7 @@ public class KraitChopperPresets {
 			.addPilotSeatSlot(0, -0.05, 2, true)
 			.addSeatSlot("copilot_seat", 0, -0.4, 3.5)
 			.addEmptySlot("nose_gun", SlotType.PYLON_LIGHT, 0, -0.5, 3.75, 180)
+			.addEmptySlot("nose_tech", SlotType.PYLON_LIGHT, 0, -0.25, 4.25, 180, "nose")
 			.addEmptySlot("left_wing_1", SlotType.PYLON_HEAVY, 1.15, -0.15, 0, 180)
 			.addEmptySlot("left_wing_2", SlotType.PYLON_MED, 2.08, 0.15, 0, -90)
 			.addEmptySlot("right_wing_1", SlotType.PYLON_HEAVY, -1.15, -0.15, 0, 180)
@@ -79,11 +80,13 @@ public class KraitChopperPresets {
 	public static final VehicleStats UNARMED_KRAIT_CHOPPER = VehicleStats.Builder
 			.createFromCopy(DSCombatMod.MODID, "krait_chopper_unarmed", EMPTY_KRAIT_CHOPPER)
 			.setCraftable()
+			.setSlotItem("nose_tech", ModItems.GIMBAL_CAMERA.getId())
 			.setSlotItem("internal_1", ModItems.C12_ENGINE.getId())
 			.setSlotItem("internal_2", ModItems.C12_ENGINE.getId())
 			.setSlotItem("internal_3", ModItems.HEAVY_FUEL_TANK.getId(), true)
 			.addIngredient(ModItems.C12_ENGINE.getId(), 2)
 			.addIngredient(ModItems.HEAVY_FUEL_TANK.getId())
+			.addIngredient(ModItems.GIMBAL_CAMERA.getId())
 			.build();
 	
 	public static final VehicleStats DEFAULT_KRAIT_CHOPPER = VehicleStats.Builder
